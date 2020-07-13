e3ae4343c85c0142fbf3c343b04fb6ee89806929
unknown <alsgks1218@naver.com>
2020-07-13 13:44:04 +0900

    basic component scafolding

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PageHeader></PageHeader>
+    <PageLists></PageLists>
+    <PageDetail></PageDetail>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PageHeader from "./components/PageHeader.vue";
+import PageLists from "./components/PageLists.vue";
+import PageDetail from "./components/PageDetail.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    PageHeader,
+    PageLists,
+    PageDetail
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/PageDetail.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>page detail</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
